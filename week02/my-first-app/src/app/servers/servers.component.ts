e6b2b0f4b1c4a9cede8b875db41e3fde2b81bd0f
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-04-26 21:52:23 -0600

    Everything working before tutorial assignment

--- week02/my-first-app/src/app/servers/servers.component.ts
+++ week02/my-first-app/src/app/servers/servers.component.ts
@@ -23,7 +23,8 @@ export class ServersComponent implements OnInit {
   ngOnInit() {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: any) {

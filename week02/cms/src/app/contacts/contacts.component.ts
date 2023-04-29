dd116efebc5b718873d304fb4810e68fa5ec31ee
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-04-29 10:37:06 -0600

    Worling on this weeks assignment

---
+++ week02/cms/src/app/contacts/contacts.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cms-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css']
+})
+export class ContactsComponent{
+}

e7c04ec5096592b13062739fd0d5071a5f038f92
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-04-29 00:00:03 -0600

    Completed the tutorial practice

--- week02/my-first-app/src/app/app.component.ts
+++ week02/my-first-app/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css'],
+
   // This below is inline
   styles: [`
   h3 {
@@ -12,5 +13,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Icka\'s Playground';
+
+  showSecret = false;
+  log: any =[];
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date);
+
+  }
 }
 

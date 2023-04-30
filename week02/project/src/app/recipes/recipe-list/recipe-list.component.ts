970c100e548d547ec5110bb577063fcd5061a95e
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-04-30 15:00:58 -0600

    Project basics complete & working as the Tutorial

--- week02/project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ week02/project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://www.averiecooks.com/wp-content/uploads/2021/01/garlicbutterchicken-5.jpg'
+    ),
+    new Recipe(
+      'A Test2 Recipe',
+      'This is simply a test2',
+      'https://www.averiecooks.com/wp-content/uploads/2021/01/garlicbutterchicken-5.jpg'
+    ),
+  ];
 
+  constructor() {}
+
+  ngOnInit() {}
 }

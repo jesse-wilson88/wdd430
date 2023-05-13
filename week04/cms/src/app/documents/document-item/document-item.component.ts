9c94407788d58ea7d3101e586ad1620c3289054f
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-05-12 22:17:14 -0600

    Everything is working up to the Dropdown part

--- week04/cms/src/app/documents/document-item/document-item.component.ts
+++ week04/cms/src/app/documents/document-item/document-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-item',
@@ -6,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-item.component.css'],
 })
 export class DocumentItemComponent implements OnInit {
+  @Input() document: Document;
+  @Output() documentSelected = new EventEmitter<void>();
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() { }
+  
+  onSelectedDocument() {
+    this.documentSelected.emit();
+  }
 }

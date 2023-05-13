1668af18ea11dcaea990fe4ed21ec2eaeff51359
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-05-12 22:54:19 -0600

    Everything is working properly in CMS Project wk4

--- week04/cms/src/app/app.module.ts
+++ week04/cms/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { MessageItemComponent } from './messages/message-item/message-item.component';
 import { MessageEditComponent } from './messages/message-edit/message-edit.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { MessageListComponent } from './messages/message-list/message-list.compo
     MessageItemComponent,
     MessageEditComponent,
     MessageListComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],

557d2c5549ff1677c872e4587012d6fd72e2bfb6
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-05-26 00:22:06 -0600

    Tutorial Configuring Route Parameters is working

--- week06/project/src/app/recipes/recipe.service.ts
+++ week06/project/src/app/recipes/recipe.service.ts
@@ -24,8 +24,12 @@ export class RecipeService {
 
   constructor(private slService: ShoppingListService) {}
 
-  getRecipe() {
+  getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(id: number) {
+    return this.recipes[id];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {

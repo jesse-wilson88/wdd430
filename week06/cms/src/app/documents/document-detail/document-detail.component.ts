0fdc4bff9a9c7b298bffc16e6b58a6ae50ae0bfb
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-05-26 23:42:31 -0600

    Implement the View Button is working

--- week06/cms/src/app/documents/document-detail/document-detail.component.ts
+++ week06/cms/src/app/documents/document-detail/document-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Document } from '../document.model';
 import { DocumentService } from '../document.service';
+import { WindRefService } from 'src/app/wind-ref.service';
 
 @Component({
   selector: 'cms-document-detail',
@@ -11,18 +12,28 @@ import { DocumentService } from '../document.service';
 })
 export class DocumentDetailComponent implements OnInit {
   document: Document;
+  nativeWindow: any;
 
   constructor(
     private documentService: DocumentService,
     private router: Router,
-    private route: ActivatedRoute
-  ) {}
+    private route: ActivatedRoute,
+    private windRefService: WindRefService
+  ) {
+    this.nativeWindow = windRefService.getNativeWindow();
+  }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       const id = params['id'];
       this.document = this.documentService.getDocument(id);
     });
+  }
+
+  onView() {
+    if (this.document.url) {
+      this.nativeWindow.open(this.document.url);
+    }
   }
 }
 

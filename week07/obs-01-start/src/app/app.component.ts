71d2e4896f9b9f800e61e6b4ececbe470ba81e61
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-05-31 07:45:50 -0600

    obs-01-start is complete. Now work on project

--- week07/obs-01-start/src/app/app.component.ts
+++ week07/obs-01-start/src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  userActivated = false;
+  private activatedSub: Subscription;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe((didActivate) => {
+      this.userActivated = didActivate;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe;
+  }
 }
--- week07/obs-01-start/src/app/home/home.component.ts
+++ week07/obs-01-start/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Subscription, interval, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -19,12 +20,37 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count == 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'))
+        }
         count++;
         // observer.error();
       }, 1000);
     });
-    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
+
+    // this.firstObsSubscription = customIntervalObservable.pipe(
+    //   map((data: number) => {
+    //     return 'Round ' + (data + 1);
+    //   })
+    // );
+
+    // this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
+    this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
+      // data > 0 gives an error but it still works with no issues
+      return data > 0;
+    }), map((data: number) => {
+      return 'Round ' + (data + 1);
+    })).subscribe(data => {
       console.log(data);
+      // console.log('Round ' + (data + 1));
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
     });
   }
 

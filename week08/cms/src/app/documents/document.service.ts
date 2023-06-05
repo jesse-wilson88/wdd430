f55aa3bb4b9768c13b5c39e9ed09d33931ce361b
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-06-04 22:27:55 -0600

    Handling Radio Buttons is wotking from tutorial

--- week08/cms/src/app/documents/document.service.ts
+++ week08/cms/src/app/documents/document.service.ts
@@ -8,7 +8,6 @@ import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
   providedIn: 'root',
 })
 export class DocumentService {
-  // documentSelectedEvent = new Subject<Document>();
   documentListChangedEvent = new Subject<Document[]>();
   documents: Document[] = [];
   maxDocumentId: number;
@@ -19,12 +18,12 @@ export class DocumentService {
   }
 
   getDocuments(): Document[] {
-    console.log('Getting all documents.');
+    // console.log('Getting all documents.');
     return this.documents.slice();
   }
 
   getDocument(id: string) {
-    console.log('Getting a document.');
+    // console.log('Getting a document.');
     return this.documents.find((document) => document.id === id);
   }
 
@@ -73,7 +72,7 @@ export class DocumentService {
   }
 
   deleteDocument(document: Document) {
-    console.log('Deleting a document.');
+    // console.log('Deleting a document.');
     if (!document) {
       return;
     }

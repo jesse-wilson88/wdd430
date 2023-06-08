e4e9f08bb03dc3d7fa832412bb8fd87aad263de0
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-06-07 23:52:19 -0600

    Allowing the Deletion of Shopping List Items works

--- week08/project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ week08/project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -35,15 +35,33 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         this.slForm.setValue({
           name: this.editedItem.name,
           amount: this.editedItem.amount,
-        })
+        });
       }
     );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
+    // Resets Button to Add
+    this.editMode = false;
+    // Resets the form
+    form.reset();
+  }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
   }
 
   ngOnDestroy() {

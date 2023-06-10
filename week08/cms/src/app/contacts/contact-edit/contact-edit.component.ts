89e84957363f542ad249f53e47e208bc262600a9
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-06-10 17:44:02 -0600

    Validate the Fields in the Contact Edit Form works

--- week08/cms/src/app/contacts/contact-edit/contact-edit.component.ts
+++ week08/cms/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-edit',
   templateUrl: './contact-edit.component.html',
-  styleUrls: ['./contact-edit.component.css']
+  styleUrls: ['./contact-edit.component.css'],
 })
 export class ContactEditComponent {
+  contact: Contact;
+  onCancel() { }
+  
+  onSubmit(form: NgForm) { }
 
 }
--- week08/cms/src/app/contacts/contact.service.ts
+++ week08/cms/src/app/contacts/contact.service.ts
@@ -19,14 +19,14 @@ export class ContactService {
   }
 
   getContacts(): Contact[] {
-    console.log('Getting all contacts.');
+    // console.log('Getting all contacts.');
     return this.contacts
       .sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0))
       .slice();
   }
 
   getContact(id: string): Contact | null {
-    console.log('Getting a contact.');
+    // console.log('Getting a contact.');
     return this.contacts.find((c) => c.id === id);
   }
 
@@ -78,7 +78,7 @@ export class ContactService {
 
   // Delete contact will be called by the ContactDetailComponent Delete button
   deleteContact(contact: Contact) {
-    console.log('Deleting a contact.');
+    // console.log('Deleting a contact.');
     if (!contact) {
       return;
     }

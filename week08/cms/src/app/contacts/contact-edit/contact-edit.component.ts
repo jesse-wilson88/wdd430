9dd7e889424a9e1cebb88126fb503f3b02e4f059
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-06-10 18:46:00 -0600

    The Behavior of the Contact Edit Form is working

--- week08/cms/src/app/contacts/contact-edit/contact-edit.component.ts
+++ week08/cms/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,17 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-edit',
   templateUrl: './contact-edit.component.html',
   styleUrls: ['./contact-edit.component.css'],
 })
-export class ContactEditComponent {
+export class ContactEditComponent implements OnInit {
+  originalContact: Contact;
   contact: Contact;
-  onCancel() { }
-  
-  onSubmit(form: NgForm) { }
+  groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id: string;
 
+  constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+
+      if (!this.id) {
+        this.editMode = false;
+        return;
+      }
+
+      this.originalContact = this.contactService.getContact(this.id);
+
+      if (!this.originalContact) {
+        return;
+      }
+
+      this.editMode = true;
+
+      this.contact = JSON.parse(JSON.stringify(this.originalContact));
+
+      if (this.originalContact.group &&
+          this.originalContact.group.length > 0) {
+            this.groupContacts = JSON.parse(JSON.stringify(this.originalContact.group));
+      }
+    });
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+    // this.router.navigate(['/contacts']);
+  }
+
+  onSubmit(form: NgForm) {}
 }
--- week08/cms/src/app/documents/document-edit/document-edit.component.ts
+++ week08/cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, NgForm } from '@angular/forms';
 

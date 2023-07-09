7b5c9a1d41998b2491d35775916c58dc93cf4ef9
jesse-wilson88 <jesse.emmert88@gmail.com>
2023-07-09 00:26:05 -0600

    Updated MOCKALBUMS.ts file

--- my_project/src/app/albums/MOCKALBUMS.ts
+++ my_project/src/app/albums/MOCKALBUMS.ts
@@ -5,7 +5,7 @@ export const MOCKALBUMS: Album[] = [
   {
     id: '1',
     artist: 'Queen',
-    album: 'News Of The World',
+    title: 'News Of The World',
     albumCoverUrl:
       'https://en.wikipedia.org/wiki/News_of_the_World_%28album%29',
     releaseDate: '28 Oct 1977',
@@ -17,6 +17,24 @@ export const MOCKALBUMS: Album[] = [
       {
         trackNumber: '2',
         songTitle: 'We Are The Champions',
+      },
+    ],
+  },
+  {
+    id: '2',
+    artist: 'Queen',
+    title: 'A Night At The Opera',
+    albumCoverUrl:
+      'https://en.wikipedia.org/wiki/A_Night_at_the_Opera_%28Queen_album%29',
+    releaseDate: '21 Nov 1975',
+    songs: [
+      {
+        trackNumber: '1',
+        songTitle: 'Death on Two Legs (Dedicated to...)',
+      },
+      {
+        trackNumber: '2',
+        songTitle: 'Lazing on a Sunday Afternoon',
       },
     ],
   },
